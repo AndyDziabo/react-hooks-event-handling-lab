1da7211e9e84c57766403c22d478c66938277bb2
Andy Dziabo <jameszabo@yahoo.com>
2022-08-30 09:04:00 -0400

    commit tree 02370ce037d88d3ad5651d8dc58d82d3cdfb4de7

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("I submit");
+    }
+
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password" name="password" placeholder="Enter password..." onChange={() => console.log('Entering password...')} />
+                <button onClick={handleSubmit}>Login</button>
+            </form>
+        </div>
     )
 }
 
